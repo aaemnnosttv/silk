0311e76239ca4e3b75ad04b6c6961a205cdb1c19
Evan Mattson <me@aaemnnost.tv>
2016-01-20 11:15:59 -0500

    more Hook methods

--- src/Hook.php
+++ src/Hook.php
@@ -8,6 +8,13 @@ class Hook
 
     protected $callback;
 
+    protected $priority = 10;
+
+    protected $iterations;
+
+    protected $maxIterations;
+
+
     /**
      * [make description]
      * @param  [type] $handle [description]
@@ -39,13 +46,116 @@ class Hook
     }
 
     /**
-     * [listen description]
+     * Set the hook in WP
      * @return [type] [description]
      */
     public function listen()
     {
-        add_filter($this->handle, $this->callback);
+        add_filter($this->handle, [$this, 'mediateCallback'], $this->priority, 100);
 
         return $this;
+    }
+
+    /**
+     * Unset the hook in WP
+     * @return [type] [description]
+     */
+    public function remove()
+    {
+        remove_filter($this->handle, [$this, 'mediateCallback'], $this->priority);
+
+        return $this;
+    }
+
+    /**
+     * [mediateCallback description]
+     * @return [type] [description]
+     */
+    public function mediateCallback($given = null)
+    {
+        if ($this->hasExceededIterations()) {
+            return $given;
+        }
+
+        $this->iterations++;
+
+        return $this->invokeCallback(func_get_args());
+    }
+
+    /**
+     * [invokeCallback description]
+     * @param  [type] $arguments [description]
+     * @return [type]            [description]
+     */
+    protected function invokeCallback($arguments)
+    {
+        $arguments = array_slice($arguments, 0, $this->getCallbackParameterCount());
+
+        return call_user_func_array($this->callback, $arguments);
+    }
+
+    /**
+     * [once description]
+     * @return [type] [description]
+     */
+    public function once()
+    {
+        $this->onlyXtimes(1);
+
+        return $this;
+    }
+
+    /**
+     * [onlyXtimes description]
+     * @param  [type] $times [description]
+     * @return [type]        [description]
+     */
+    public function onlyXtimes($times)
+    {
+        $this->maxIterations = (int) $times;
+
+        return $this;
+    }
+
+    /**
+     * Prevent the callback from being triggered again
+     * @return [type] [description]
+     */
+    public function bypass()
+    {
+        $this->onlyXtimes(0);
+
+        return $this;
+    }
+
+    /**
+     * [withPriority description]
+     * @param  [type] $priority [description]
+     * @return [type]           [description]
+     */
+    public function withPriority($priority)
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    /**
+     * [getCallbackParameterCount description]
+     * @return [type] [description]
+     */
+    protected function getCallbackParameterCount()
+    {
+        return (new \ReflectionFunction($this->callback))
+            ->getNumberOfParameters();
+    }
+
+    /**
+     * [hasExceededIterations description]
+     * @return boolean [description]
+     */
+    protected function hasExceededIterations()
+    {
+        return ($this->maxIterations > -1) && ($this->iterations >= $this->maxIterations);
     }
 }
--- tests/unit/HookTest.php
+++ tests/unit/HookTest.php
@@ -21,6 +21,8 @@ class HookTest extends WP_UnitTestCase
     {
         $hook = Hook::make('asdf')
             ->setCallback(function () {})
+            ->withPriority(99)
+            ->once()
             ->listen();
 
         $this->assertInstanceOf(Hook::class, $hook);
@@ -46,5 +48,119 @@ class HookTest extends WP_UnitTestCase
         apply_filters('some_action', 'Filter this!');
 
         $this->assertEquals($data, 'Filter this!');
+    }
+
+    /**
+     * @test
+     */
+    public function it_listens_on_the_priority_we_set()
+    {
+        $data = 'this is passed by reference to the callback';
+
+        $iterate = function ($value) {
+            return ++$value;
+        };
+        add_filter('filterme', $iterate, 1);
+        add_filter('filterme', $iterate, 2);
+        add_filter('filterme', $iterate, 3);
+        add_filter('filterme', $iterate, 4);
+        // check here
+        add_filter('filterme', $iterate, 6);
+        add_filter('filterme', $iterate, 7);
+        add_filter('filterme', $iterate, 8);
+
+        Hook::make('filterme')
+            ->setCallback(function ($value) use (&$data) {
+                $data = $value;
+            })
+            ->withPriority(5)
+            ->listen();
+
+        apply_filters('filterme', 1);
+
+        $this->assertEquals(5, $data);
+    }
+
+    /**
+     * @test
+     */
+    public function it_passes_the_correct_number_of_arguments_to_the_callback_automatically()
+    {
+        $arguments_count = null;
+
+        $hook = Hook::make('testing_arguments_passed')
+            ->setCallback(function ($one, $two, $three) use (&$arguments_count) {
+                $arguments_count = func_num_args();
+            })
+            ->listen();
+
+        do_action('testing_arguments_passed', 1, 2, 3);
+
+        $this->assertEquals(3, $arguments_count);
+
+        $arguments_count = null;
+        $hook->setCallback(function ($one, $two) use (&$arguments_count) {
+            $arguments_count = func_num_args();
+        });
+
+        do_action('testing_arguments_passed', 1, 2, 3);
+
+        $this->assertEquals(2, $arguments_count);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_be_set_to_only_fire_once()
+    {
+        $count = 0;
+
+        Hook::make('only_once_test')
+            ->setCallback(function () use (&$count) {
+                $count++;
+            })
+            ->once()
+            ->listen();
+
+        do_action('only_once_test');
+        do_action('only_once_test');
+        do_action('only_once_test');
+
+        $this->assertEquals(1, $count);
+
+
+        Hook::make('only_once_filtered')
+            ->setCallback(function ($value) {
+                $value *= 2;
+                return $value;
+            })
+            ->once()
+            ->listen();
+
+        $result = apply_filters('only_once_filtered', 1);
+        $result = apply_filters('only_once_filtered', $result);
+        $result = apply_filters('only_once_filtered', $result);
+
+        $this->assertEquals(2, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_remove_its_hook_if_needed()
+    {
+        $hook = Hook::make('remove_this_test')
+            ->setCallback(function () {
+                throw new Exception('Test failed!');
+            })
+            ->listen();
+
+        $this->assertTrue(has_action('remove_this_test'));
+
+        $hook->remove();
+
+        $this->assertFalse(has_action('remove_this_test'));
+
+        do_action('remove_this_test');
     }
 }
