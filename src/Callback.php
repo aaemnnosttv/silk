4bf99e71192dd6bffc5c697e22918cb6c73437a0
Evan Mattson <me@aaemnnost.tv>
2016-05-28 00:36:58 -0400

    update callback & add docs

--- src/Callback.php
+++ src/Callback.php
@@ -8,12 +8,28 @@ class Callback
 
     protected $callbackParamCount;
 
+    /**
+     * Create a new Callback instance
+     *
+     * @param callable $target The callback to wrap
+     */
     public function __construct(callable $target)
     {
-        $this->target = $this->normalizeSyntax($target);
+        $this->target = static::normalizeSyntax($target);
     }
 
-    protected function normalizeSyntax($callback)
+    /**
+     * Normalize the callable syntax
+     *
+     * Converts string static class method to standard callable array.
+     *
+     * @param  mixed $callback  Target callback
+     *
+     * @return mixed Closure    Anonymous function
+     *               array      Class method
+     *               string     Function
+     */
+    public static function normalizeSyntax(callable $callback)
     {
         if (is_string($callback) && false !== strpos($callback, '::')) {
             $callback = explode('::', $callback);
@@ -22,21 +38,42 @@ class Callback
         return $callback;
     }
 
+    /**
+     * Call the target callable
+     *
+     * @return mixed  Returns the return value of the callback, or FALSE on error.
+     */
     public function call()
     {
         return $this->callArray(func_get_args());
     }
 
-    public function callArray($arguments)
+    /**
+     * Call the target callable, with an array of arguments
+     *
+     * @param  array $arguments  The parameters to be passed to the callback, as an indexed array.
+     * @return mixed             Returns the return value of the callback, or FALSE on error.
+     */
+    public function callArray(array $arguments = [])
     {
         return call_user_func_array($this->target, $arguments);
     }
 
+    /**
+     * Get the target callable
+     *
+     * @return mixed  The normalized callable
+     */
     public function get()
     {
         return $this->target;
     }
 
+    /**
+     * Get the corresponding Reflection instance for the target callable
+     *
+     * @return \ReflectionFunctionAbstract
+     */
     public function reflect()
     {
         if ($this->target instanceof \Closure
