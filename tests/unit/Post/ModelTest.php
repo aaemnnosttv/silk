920622031a2f55e3b97558582c72b6fe8ce8c699
Evan Mattson <me@aaemnnost.tv>
2016-07-01 14:10:20 +0400

    add test for post type as const + trait

--- tests/unit/Post/ModelTest.php
+++ tests/unit/Post/ModelTest.php
@@ -11,6 +11,7 @@ class ModelTest extends PHPUnit_Framework_TestCase
     function it_has_a_method_for_getting_the_post_type_id()
     {
         $this->assertSame('event', ModelTestEvent::postTypeId());
+        $this->assertSame('event', ModelTestEventTrait::postTypeId());
         $this->assertSame('model_test_post_type', ModelTestPostType::postTypeId());
         $this->assertSame('dinosaur', Dinosaur::postTypeId());
     }
@@ -46,5 +47,16 @@ class Dinosaur extends Model
  */
 class ModelTestPostType extends Model
 {
+    use Silk\Post\ClassNameAsPostType;
+}
+
+/**
+ * Models post with post_type 'model_test_post_type'
+ */
+class ModelTestEventTrait extends ModelTestEvent
+{
+    /**
+     * Here the trait is overriden by the constant
+     */
     use Silk\Post\ClassNameAsPostType;
 }
