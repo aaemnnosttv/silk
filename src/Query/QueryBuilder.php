ca5995c589861283f461afbf93d900f11201360d
Evan Mattson <me@aaemnnost.tv>
2016-07-06 16:58:40 +0400

    add missing magic methods to QueryBuilder trait

--- src/Query/QueryBuilder.php
+++ src/Query/QueryBuilder.php
@@ -22,4 +22,34 @@ trait QueryBuilder
     {
         return (new static)->newQuery();
     }
+
+    /**
+     * Handle dynamic static method calls on the model class.
+     *
+     * Proxies calls to direct method calls on a new instance
+     *
+     * @param string $method
+     * @param array $arguments
+     *
+     * @return mixed
+     */
+    public static function __callStatic($method, array $arguments)
+    {
+        return call_user_func_array([new static, $method], $arguments);
+    }
+
+    /**
+     * Handle dynamic method calls into the model.
+     *
+     * @param  string $method
+     * @param  array $arguments
+     *
+     * @return mixed
+     */
+    public function __call($method, $arguments)
+    {
+        $query = $this->newQuery();
+
+        return call_user_func_array([$query, $method], $arguments);
+    }
 }
