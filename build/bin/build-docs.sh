99ec93b984f34c90f13899980dab51d9d4b3355b
Evan Mattson <me@aaemnnost.tv>
2016-07-06 11:15:48 +0400

    build: try force with sami on master

--- build/bin/build-docs.sh
+++ build/bin/build-docs.sh
@@ -5,7 +5,7 @@ curl -O http://get.sensiolabs.org/sami.phar
 
 # Because Travis only clones a single branch, we only want to build everything on master.
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    php sami.phar update build/sami-config.php -v
+    php sami.phar update build/sami-config.php -v --force
 else
     php sami.phar update build/sami-config.php -vvv --only-version="$TRAVIS_BRANCH"
 fi
