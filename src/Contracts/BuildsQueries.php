d70b92b68cb9b2c2250a39aa21269067cef5a8e4
Evan Mattson <me@aaemnnost.tv>
2016-07-12 18:31:51 +0400

    remove model methods from BuildsQueries interface

--- src/Contracts/BuildsQueries.php
+++ src/Contracts/BuildsQueries.php
@@ -7,22 +7,6 @@ use Illuminate\Support\Collection;
 interface BuildsQueries
 {
     /**
-     * Set the model for this query.
-     *
-     * @param mixed $model
-     *
-     * @return $this
-     */
-    public function setModel($model);
-
-    /**
-     * Get the model.
-     *
-     * @return mixed Model
-     */
-    public function getModel();
-
-    /**
      * Get the results of the query.
      *
      * @return Collection
