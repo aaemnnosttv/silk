da98c9b4367e1eaab099e880118f057678243c37
Evan Mattson <me@aaemnnost.tv>
2016-07-15 18:18:38 +0400

    fix docblock in Hook

--- src/Event/Hook.php
+++ src/Event/Hook.php
@@ -119,10 +119,10 @@ class Hook
     /**
      * Control invocation of the callback.
      *
-     * @param $given  The first argument passed to the callback.
-     *                Needed to return for filters.
+     * @param mixed $given  The first argument passed to the callback.
+     *                      Needed to return for filters.
      *
-     * @return mixed  Returned value from Callback
+     * @return mixed        Returned value from Callback
      */
     public function mediateCallback($given = null)
     {
