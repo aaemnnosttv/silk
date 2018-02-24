f84666eb88655cbc9d112b1430f6d10b6b304690
Evan Mattson <me@aaemnnost.tv>
2018-02-24 23:49:06 +0200

    dynamically extend our Collection for php56 compat

--- src/Support/Collection.php
+++ src/Support/Collection.php
@@ -2,7 +2,22 @@
 
 namespace Silk\Support;
 
-class Collection extends \Tightenco\Collect\Support\Collection
-{
+/**
+ * The Tightenco namespaced Collection class has only been available since 5.5.33
+ * with aliases for the previous Illuminate\Support class for backwards compat.
+ *
+ * For PHP 5.6, v5.4 of the library will be installed, thus the namespaced version
+ * won't be available.
+ */
 
+// @codeCoverageIgnoreStart
+if (class_exists('Tightenco\Collect\Support\Collection')) {
+    class Collection extends \Tightenco\Collect\Support\Collection
+    {
+    }
+} else {
+    class Collection extends \Illuminate\Support\Collection
+    {
+    }
 }
+// @codeCoverageIgnoreEnd
