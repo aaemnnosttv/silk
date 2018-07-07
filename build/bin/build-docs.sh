86be6695a11f5021efa670bb20bdaf4ba3e73c28
Evan Mattson <me@aaemnnost.tv>
2018-07-07 21:25:32 +0200

    ensure working directory is clean before building docs

--- build/bin/build-docs.sh
+++ build/bin/build-docs.sh
@@ -5,6 +5,9 @@ if [ ! -f sami.phar ]; then
     curl -O http://get.sensiolabs.org/sami.phar
 fi
 
+# Ensure the working directory is clean
+git reset --hard
+
 # Because Travis only clones a single branch, we only want to build everything on master.
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     php sami.phar update build/sami-config.php -v --force
