03ce83d2e8330476be4e395923c8c71e16e4f43b
Evan Mattson <me@aaemnnost.tv>
2018-07-07 12:22:31 +0200

    update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,5 @@
 <?php
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
+require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
-
-require_once $_tests_dir . '/includes/functions.php';
-
-require_once realpath(__DIR__ . '/../vendor/autoload.php');
-
-require $_tests_dir . '/includes/bootstrap.php';
+require(getenv('WP_PHPUNIT__DIR') . '/includes/bootstrap.php');
